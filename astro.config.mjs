3ddb1f07427126173945df8469520dd79401fc66
NikSne <commits@niksne.ru>
2024-12-03 08:26:39 +0300

    feat: allow usage of @ imports for some common directories

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://the-unnamed-nug.github.io",
+  base: "/sherpa",
   integrations: [tailwind()],
 });
