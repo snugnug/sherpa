58b16207b4c18063789ae6317064872258dba44f
damii <deanam1507@gmail.com>
2024-12-03 21:33:44 +1100

    Fix i18n support, update to astro v5 beta

---
+++ src/content.config.js
@@ -0,0 +1,14 @@
+// 1. Import utilities from `astro:content`
+import { z, defineCollection } from "astro:content";
+import { glob } from 'astro/loaders'; // Not available with legacy API
+
+// 2. Define your collection(s)
+const docs = defineCollection({
+  loader: glob({ pattern: '**\/[^_]*.md', base: "./src/content/docs" }),
+  schema: z.object({
+    title: z.string().optional(),
+    tags: z.array(z.string()).optional(),
+  }),
+});
+
+export const collections = { docs };
