6089aad0afe6f5d2ab92b2b4e8cc73461f53e437
damii <deanam1507@gmail.com>
2024-12-03 22:13:27 +1100

    Add patches for site redirection

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,5 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://the-unnamed-nug.github.io",
-  base: "/sherpa",
   integrations: [tailwind()],
 });
--- src/utils/links.ts
+++ src/utils/links.ts
@@ -1,3 +1,2 @@
 const siteLink = (path: string) => `${import.meta.env.BASE_URL}${path}`;
-
 export { siteLink };
